b80bb0259b282badacbe484f209b32b43a0a931d
Sergey Tihon <sergey_tihon@epam.com>
2022-12-18 21:53:25 +0100

    feat: telemetry and tracing

--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@ use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes::{health_check, subscribe};
 
@@ -9,6 +10,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> std::io::Result<Server> {
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
