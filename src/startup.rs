063180c6c22b6ff0c9f5de6567fbfa0a525c0c6c
Sergey Tihon <sergey_tihon@epam.com>
2022-12-22 00:00:08 +0100

    feat: email_client prototype

--- src/startup.rs
+++ src/startup.rs
@@ -1,19 +1,32 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpServer};
+use actix_web::{
+    dev::Server,
+    web::{self, Data},
+    App, HttpServer,
+};
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
 
-use crate::routes::{health_check, subscribe};
+use crate::{
+    email_client::EmailClient,
+    routes::{health_check, subscribe},
+};
 
-pub fn run(listener: TcpListener, db_pool: PgPool) -> std::io::Result<Server> {
-    let db_pool = web::Data::new(db_pool);
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+    email_client: EmailClient,
+) -> std::io::Result<Server> {
+    let db_pool = Data::new(db_pool);
+    let email_client = Data::new(email_client);
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
+            .app_data(email_client.clone())
     })
     .listen(listener)?
     .run();
